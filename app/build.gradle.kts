1b0b5b5b1f6d6cc518c6da3dc8ad9f4622d0710c
mg3994 <manishgautammg7@yahoo.com>
2024-04-20 09:24:29 +0530

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.antinna.testingnewandroidapi"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.antinna.testingnewandroidapi"
